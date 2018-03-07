101fb6cc9587c056466eeb898e7b242e34e2c7a9
Sam Pham <sam.h.pham@gmail.com>
2018-03-07 17:22:39 +0000

    A fix

--- pi_files/mqtt_publish_test_v2.py
+++ pi_files/mqtt_publish_test_v2.py
@@ -2,15 +2,13 @@ import paho.mqtt.client as mqtt
 import time
 data = "Hello from Raspberry Pi!"
 while True:
-
-
-print(data)
-try:
-    client=mqtt.Client()
-    client.username_pw_set("qufzpimd","ra44TqXIg1PZ")
-    client.connect("m23.cloudmqtt.com",10952,60)
-    client.publish("nhm",data)
-    time.sleep(1)
-except KeyboardInterrupt:
-    print("end")
-    client.disconnect()
+    print(data)
+    try:
+        client=mqtt.Client()
+        client.username_pw_set("qufzpimd","ra44TqXIg1PZ")
+        client.connect("m23.cloudmqtt.com",10952,60)
+        client.publish("nhm",data)
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print("end")
+        client.disconnect()
