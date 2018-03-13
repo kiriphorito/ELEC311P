6b7a248ba883aeaf3e91d5898faf7b177b9411f0
Sam Pham <sam.h.pham@gmail.com>
2018-03-13 23:46:41 +0000

    Fix

--- pi_files/mcp3008_mic_test.py
+++ pi_files/mcp3008_mic_test.py
@@ -38,9 +38,9 @@ while True:
         mic_voltage = mcp.read_adc(0)
         if (mic_voltage < 1024):
             if mic_voltage > mic_voltage_max:
-                mic_voltage_min = mic_voltage
-            elif mic_voltage < mic_voltage_min:
                 mic_voltage_max = mic_voltage
+            elif mic_voltage < mic_voltage_min:
+                mic_voltage_min = mic_voltage
         print(mic_voltage)
 
     signal_range = mic_voltage_max - mic_voltage_min
