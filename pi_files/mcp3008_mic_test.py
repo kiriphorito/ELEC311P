c385ce562a95baeaff1b7c4f62b5ee3258673255
Sam Pham <sam.h.pham@gmail.com>
2018-03-18 00:59:02 +0000

    Fix

--- pi_files/mcp3008_mic_test.py
+++ pi_files/mcp3008_mic_test.py
@@ -59,10 +59,11 @@ while True:
         #values[i] = mcp.read_adc(i)
     # Print the ADC values.
     #print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
-    print(signal_range)
+    # print(signal_range)
 
     range = 10
-    for x in range(0, signal_range//range):
+    steps = signal_range // range
+    for x in range(0, steps):
         print("X", end="", flush=True)
     print()
     # time.sleep(1/SAMPLE_WINDOW)
